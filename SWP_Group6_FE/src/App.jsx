61823a302d7cea0198a9bc812330bb0ba8cb8c88
nhatt <nhattran2016.nt@gmail.com>
2025-02-22 17:41:53 +0700

    Added temp edit page

--- SWP_Group6_FE/src/App.jsx
+++ SWP_Group6_FE/src/App.jsx
@@ -2,8 +2,10 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Hg from "./components/HomePage/hg";
 import ProtectedRoute from "./components/protected-route";
 import Dashboard from "./components/HomePage/DashBoard";
+import EditAccount from "./components/EditAccount";
 // import AuthPage from "./components/Login/loginv2";
 import Login from "./components/Login/Login";
+
 
 
 function App() {
@@ -24,6 +26,11 @@ function App() {
       path: "/dashboard",
       element: <Dashboard />,
     },
+    {
+      path: "/editAccount",
+      element: <EditAccount />,
+    }
+
   ]);
 
   return <RouterProvider router={router} />;
---
+++ SWP_Group6_FE/src/components/EditAccount.jsx
@@ -0,0 +1,18 @@
+import { TextField } from "@mui/material";
+
+const EditAccount = () => {
+    return(
+        <>
+            <h3> Edit page </h3>
+            <div>
+            <TextField id="filled-basic" label="Filled" variant="filled" />
+            </div>
+            
+        
+        </>
+        
+    )
+    
+};
+
+export default EditAccount;
