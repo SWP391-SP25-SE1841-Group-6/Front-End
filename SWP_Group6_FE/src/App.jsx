9db4c3d058e76cd72b65a8f5d94013231a5eab11
nhatt <nhattran2016.nt@gmail.com>
2025-03-05 13:58:26 +0700

    WIP edit page + cors fix

--- SWP_Group6_FE/src/App.jsx
+++ SWP_Group6_FE/src/App.jsx
@@ -2,13 +2,14 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Hg from "./components/HomePage/hg";
 import ProtectedRoute from "./components/protected-route";
 import Dashboard from "./components/HomePage/DashBoard";
-import EditAccount from "./components/EditAccount";
+import EditAccount from "./components/EditAccount/EditAccount"
 // import AuthPage from "./components/Login/loginv2";
 import Login from "./components/Login/Login";
 
 
 
 function App() {
+  
   const router = createBrowserRouter([
     {
       path: "/homepage",
--- SWP_Group6_FE/src/components/config/axios.js
+++ SWP_Group6_FE/src/components/config/axios.js
@@ -1,6 +1,6 @@
 
 import axios from "axios";
-const baseUrl = "http://localhost:5121/api";
+const baseUrl = "http://localhost:5121/api/";
 
 const config = {
   baseURL: baseUrl,
